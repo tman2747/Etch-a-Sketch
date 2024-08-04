a2d0ae1baf2b47936bcfb2d7c128a33601ca9e37
Triston Land <tristonland@gmail.com>
2024-08-04 15:16:52 -0600

    add reset grid and generate new grid with custom sizes

--- javascript.js
+++ javascript.js
@@ -1,17 +1,49 @@
 let container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++)
+let color = "green"
+
+let height = 16
+let width = 16
+
+function generateGrid(height = 16, width = 16)
 {
-    const column = document.createElement("div")
-    container.appendChild(column)
-    for (let j = 0; j < 16; j++)
+    for (let i = 0; i < width; i++)
     {
-        const square = document.createElement("div")
-        square.classList.add("squares")
-        square.addEventListener("mouseover",()=>{
-            console.log("hover")
-            square.style.background = "green"
-        })
-        column.appendChild(square)
+        const column = document.createElement("div")
+        container.appendChild(column)
+        for (let j = 0; j < height; j++)
+        {
+            const square = document.createElement("div")
+            square.classList.add("squares")
+            square.addEventListener("mouseover", () =>
+            {
+                square.style.background = color
+            })
+            column.appendChild(square)
+        }
     }
 }
+
+function deleteGrid()
+{
+    container.innerHTML = '';
+}
+
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", ()=>{
+    deleteGrid() // or it should just reset the colors inside the grid
+    generateGrid(height,width) // or we could the last submitted value in the generate grid and have it regenerate that grid
+})
+
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", ()=>{
+    deleteGrid()
+    let inputlist = document.querySelectorAll("input")// using input list instead of id to practice using node list
+    console.log(inputlist[0].value)
+    height = inputlist[0].value
+    width = inputlist[1].value
+    generateGrid(height,width)
+
+    return // should take the input of x and y and generate a new grid with those values
+})
+generateGrid(16,16)
